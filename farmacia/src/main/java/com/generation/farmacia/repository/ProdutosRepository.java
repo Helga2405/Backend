f974d8f9fbb5fe92dae2c17601b52ef291604745
Helga <onegra138@gmail.com>
2022-05-09 16:39:43 -0300

    farmacia

--- farmacia/src/main/java/com/generation/farmacia/repository/ProdutosRepository.java
+++ farmacia/src/main/java/com/generation/farmacia/repository/ProdutosRepository.java
@@ -3,9 +3,12 @@ package com.generation.farmacia.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 import com.generation.farmacia.model.ProdutosModel;
 
+@Repository
 public interface ProdutosRepository  extends JpaRepository<ProdutosModel, Long> {
 public List<ProdutosModel> findAllByNomeContainingIgnoreCase (String nome);
 }
